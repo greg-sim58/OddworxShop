6602b283f39392e7a22aab14e7b402fe1ad673fd
greg-sim58 <greg@sim58dev.com>
2018-09-05 18:55:40 +0200

    - Create shop validation

--- OddworxShop/OddworxShop.Admin/ViewModels/ShopsViewModel.cs
+++ OddworxShop/OddworxShop.Admin/ViewModels/ShopsViewModel.cs
@@ -20,12 +20,14 @@ namespace OddworxShop.Admin.ViewModels
         [Display(Name="Description of Shop")]
         public string Description { get; set; }
 
-        [Required]
         [Display(Name="Contact Email")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string ContactEmail { get; set; }
 
         [Required]
         [Display(Name = "Contact Phone")]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone no.")]
         public string ContactPhone { get; set; }
 
         [Display(Name = "Shop Website")]
